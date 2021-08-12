7e09d7cedefd7daf49dfb20ee7497a3497641ea6
granit <granitbro@gmail.com>
2021-08-12 01:16:24 -0500

    navigation bar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,3 +4,16 @@
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
+		<nav class="navbar navbar-inverse">
+			<div class="container">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">Review Everything</a>
+				</div>
+				<div id="navbar">
+					<ul class="nav navbar-nav">
+						<li><a href="/">Home</a></li>
+						<li><a href="/about">About</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
