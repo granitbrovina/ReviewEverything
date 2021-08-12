fe792dc2109ec7fccb9361895bdbaefb4ca5f5e9
granit <granitbro@gmail.com>
2021-08-12 01:35:41 -0500

    navigation bar fixes

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,12 +7,12 @@
 		<nav class="navbar navbar-inverse">
 			<div class="container">
 				<div class="navbar-header">
-					<a class="navbar-brand" href="/~89171015/RE/">Review Everything</a>
-				</div>
-				<div id="navbar">
-					<ul class="nav navbar-nav">
-						<li><a href="/~89171015/RE/">Home</a></li>
-						<li><a href="/~89171015/RE/about">About</a></li>
+					<a class="navbar-brand" href="<?php echo base_url(); ?>">Review Everything</a>
+        		</div>
+        		<div id="navbar">
+          			<ul class="nav navbar-nav">
+						<li><a href="<?php echo base_url(); ?>">Home</a></li>
+						<li><a href="<?php echo base_url(); ?>/about">About</a></li>
 					</ul>
 				</div>
 			</div>
