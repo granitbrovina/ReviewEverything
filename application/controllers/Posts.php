cf314a5475d7b4994a00a3185d41b06d9cb2d2dd
granit <granitbro@gmail.com>
2021-08-12 18:54:36 -0500

    created comapnies page

---
+++ application/controllers/Posts.php
@@ -0,0 +1,11 @@
+<?php
+	class Posts extends CI_Controller{
+		public function index(){
+			$data['title'] = 'Companies';
+
+			$this->load->view('templates/header');
+			$this->load->view('posts/index', $data);
+			$this->load->view('templates/footer');
+			
+		}
+	}
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,10 +7,10 @@
 		<nav class="navbar navbar-inverse">
 			<div class="container">
 				<div class="navbar-header">
-					<a class="logo" href="<?php echo base_url(); ?>">Review Everything</a>
+					<a class="navbar-brand" href="<?php echo base_url(); ?>">Review Everything</a>
         		</div>
         		<div id="navbar">
-          			<ul class="pages">
+          			<ul class="nav navbar-nav">
 						<li><a href="<?php echo base_url(); ?>">Home</a></li>
 						<li><a href="<?php echo base_url(); ?>/about">About</a></li>
 					</ul>
