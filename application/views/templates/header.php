81d7e03d2fe6db3a1223ba7b65dafe761f0a74b5
granit <granitbro@gmail.com>
2021-08-12 02:57:02 -0500

    ...

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,10 +7,10 @@
 		<nav class="navbar navbar-inverse">
 			<div class="container">
 				<div class="navbar-header">
-					<a class="navbar-brand" href="<?php echo base_url(); ?>">Review Everything</a>
+					<a class="logo" href="<?php echo base_url(); ?>">Review Everything</a>
         		</div>
         		<div id="navbar">
-          			<ul class="nav navbar-nav">
+          			<ul class="pages">
 						<li><a href="<?php echo base_url(); ?>">Home</a></li>
 						<li><a href="<?php echo base_url(); ?>/about">About</a></li>
 					</ul>
