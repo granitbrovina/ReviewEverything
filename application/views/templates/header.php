48fbb7a12f6521b599881ee30905b53a0cbd1bd0
granit <granitbro@gmail.com>
2021-08-12 01:25:56 -0500

    typo fixes

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,18 +1,18 @@
 <html>
 	<head>
 		<title>Review Everything</title>
-		<link rel="stylesheet" href="https://bootswatch.com/5/flatly/bootstrap.min.css">
+		<link rel="stylesheet" href="https://bootswatch.com/5/lumen/bootstrap.min.css">
 	</head>
 	<body>
 		<nav class="navbar navbar-inverse">
 			<div class="container">
 				<div class="navbar-header">
-					<a class="navbar-brand" href="/">Review Everything</a>
+					<a class="navbar-brand" href="/~89171015/RE/">Review Everything</a>
 				</div>
 				<div id="navbar">
 					<ul class="nav navbar-nav">
-						<li><a href="/">Home</a></li>
-						<li><a href="/about">About</a></li>
+						<li><a href="/~89171015/RE/">Home</a></li>
+						<li><a href="/~89171015/RE/about">About</a></li>
 					</ul>
 				</div>
 			</div>
