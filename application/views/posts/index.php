5a2fb05881a8500fcb2b082a529e3e2707eaa85e
granit <granitbro@gmail.com>
2021-08-13 18:11:34 -0500

    further fixing of company/posts

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,4 +1,5 @@
 <?php foreach($posts as $post) : ?>
 	<h3><?php echo $post['name']; ?></h3>
 	<?php echo $post['summary']; ?>
+	<p><a href="<?php echo site_url('/posts'.$post['slug']); ?>">Read More</a></p>
 <?php endforeach; ?>
