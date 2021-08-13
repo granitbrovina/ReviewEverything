400968691946a4b36f25586dd107bfc8121ce83a
granit <granitbro@gmail.com>
2021-08-13 18:21:25 -0500

    further fixing of company/posts

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,17 +5,17 @@
 	</head>
 	<body>
 		<nav class="navbar navbar-inverse">
-			
+			<div class="container">
 				<div class="navbar-header">
 					<a class="navbar-brand" href="<?php echo base_url(); ?>">Review Everything</a>
         		</div>
         		<div id="navbar">
           			<ul class="nav navbar-nav">
-						<li><a href="<?php echo base_url(); ?>">Home</a></li>
-						<li><a href="<?php echo base_url(); ?>posts">Browse</a></li>
-						<li><a href="<?php echo base_url(); ?>about">About</a></li>
+						<li class="nav-item"><a href="<?php echo base_url(); ?>">Home</a></li>
+						<li class="nav-item"><a href="<?php echo base_url(); ?>posts">Browse</a></li>
+						<li class="nav-item"><a href="<?php echo base_url(); ?>about">About</a></li>
 					</ul>
 				</div>
-			
+			</div>
 		</nav>
 		<div class="container">
