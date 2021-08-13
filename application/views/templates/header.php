c87a06ce9f81e19379ab739466a34aaff62af4a5
granit <granitbro@gmail.com>
2021-08-13 18:26:19 -0500

    further fixing of company/posts

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,13 +9,20 @@
 				<div class="navbar-header">
 					<a class="navbar-brand" href="<?php echo base_url(); ?>">Review Everything</a>
         		</div>
-        		<div id="navbar">
-          			<ul class="nav navbar-nav">
+        		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				    <div class="navbar-nav">
+				      <a class="nav-item nav-link" href="<?php echo base_url(); ?>">Home</a>
+				      <a class="nav-item nav-link" href="<?php echo base_url(); ?>posts">Browse</a>
+				      <a class="nav-item nav-link" href="<?php echo base_url(); ?>about">About</a>
+				    </div>
+				</div>
+        		<!--<div id="navbar">
+          			<ul class="navbar-nav">
 						<li class="nav-item"><a href="<?php echo base_url(); ?>">Home</a></li>
 						<li class="nav-item"><a href="<?php echo base_url(); ?>posts">Browse</a></li>
 						<li class="nav-item"><a href="<?php echo base_url(); ?>about">About</a></li>
 					</ul>
-				</div>
+				</div>-->
 			</div>
 		</nav>
 		<div class="container">
