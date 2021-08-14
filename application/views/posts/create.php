b0574d270d35d8e9ca142c231abae211f9bb9d3d
granit <granitbro@gmail.com>
2021-08-13 19:51:37 -0500

    fixing up signup page for companies

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -2,19 +2,19 @@
 
 <div class="form-group">
     <label>Company name</label>
-    <input type="text" class="form-control" name="title" placeholder="Add Title">
+    <input type="text" class="form-control" name="title" placeholder="Add name">
   </div>
   <div class="form-group">
     <label>Summary</label>
-    <textarea id="editor1" class="form-control" name="body" placeholder="Add Body"></textarea>
+    <textarea id="editor1" class="form-control" name="body" placeholder="Add summary"></textarea>
   </div>
   <div class="form-group">
     <label>Website</label>
-    <textarea id="editor1" class="form-control" name="body" placeholder="Add Body"></textarea>
+    <textarea id="editor1" class="form-control" name="body" placeholder="Add website url"></textarea>
   </div>
   <div class="form-group">
     <label>Contact email</label>
-    <textarea id="editor1" class="form-control" name="body" placeholder="Add Body"></textarea>
+    <textarea id="editor1" class="form-control" name="body" placeholder="Add contact email"></textarea>
   </div>
   <div class="form-group">
 	  <label>Upload logo</label>
