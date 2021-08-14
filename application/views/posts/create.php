21a117bd77ebe1d5c11f5a434397267e16bb25c8
granit <granitbro@gmail.com>
2021-08-14 17:07:09 -0500

    fixing image upload functionality

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -21,7 +21,7 @@
   </div>
   <div class="form-group">
 	  <label>Upload logo</label>
-	  <input type="file" name="postimage" size="20">
+	  <input type="file" name="userfile" size="20">
   </div>
   <button type="submit" class="btn btn-default">Submit</button>
 </form>
