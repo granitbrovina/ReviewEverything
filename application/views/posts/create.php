2fc20bbfcb87d4d878855ecc73b88b35d3702c84
granit <granitbro@gmail.com>
2021-08-13 19:47:44 -0500

    fixing up signup page for companies

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -1,1 +1,24 @@
 <h2><?= $title ?></h2>
+
+<div class="form-group">
+    <label>Company name</label>
+    <input type="text" class="form-control" name="title" placeholder="Add Title">
+  </div>
+  <div class="form-group">
+    <label>Summary</label>
+    <textarea id="editor1" class="form-control" name="body" placeholder="Add Body"></textarea>
+  </div>
+  <div class="form-group">
+    <label>Website</label>
+    <textarea id="editor1" class="form-control" name="body" placeholder="Add Body"></textarea>
+  </div>
+  <div class="form-group">
+    <label>Contact email</label>
+    <textarea id="editor1" class="form-control" name="body" placeholder="Add Body"></textarea>
+  </div>
+  <div class="form-group">
+	  <label>Upload logo</label>
+	  <input type="file" name="userfile" size="20">
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
