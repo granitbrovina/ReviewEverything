f5290a898346b4977f18ed0e1f901905e1d8bc60
granit <granitbro@gmail.com>
2021-08-14 20:06:09 -0500

    fixing how company logo is displayed

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -2,7 +2,7 @@
 	<h3><?php echo $post['name']; ?></h3>
 	<div class="row">
 		<div class="col-md-3">
-			<img src="<?php echo site_url(); ?>assets/images/<?php echo $post['logo']; ?>">
+			<img src="<?php echo site_url(); ?>assets/images/<?php echo $post['logo']; ?>" width="300" height="300">
 		</div>
 		<div class="col-md-9">
 			<?php echo $post['summary']; ?>
