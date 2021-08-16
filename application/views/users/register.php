acb8d292b255aef5bd10c7789ed1d072c073a663
granit <granitbro@gmail.com>
2021-08-16 04:28:55 -0500

    creating registration form

---
+++ application/views/users/register.php
@@ -0,0 +1,18 @@
+<h2><?= $title; ?></h2>
+<?php echo validation_errors(); ?>
+
+<?php echo form_open('users/register'); ?>
+	<div class="form-group">
+		<label>Name</label>
+		<input type="text" class="form-control" name="name" placeholder="Name">
+		<label>Email</label>
+		<input type="text" class="form-control" name="email" placeholder="Email">	
+		<label>Username</label>
+		<input type="text" class="form-control" name="username" placeholder="Username">	
+		<label>Password</label>
+		<input type="password" class="form-control" name="password" placeholder="Password">
+		<label>Confirm Password</label>
+		<input type="password" class="form-control" name="password2" placeholder="Confirm Password">
+	</div>
+	<button type="submit" class="btn btn-primary">Sign-up</button>
+<?php echo form_close(); ?>
