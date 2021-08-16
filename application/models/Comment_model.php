6b6102f8ec4292ecbca2d27ac6c36b922d87dd70
granit <granitbro@gmail.com>
2021-08-16 01:19:10 -0500

    patching and fixing review/rating capability

--- application/models/Comment_model.php
+++ application/models/Comment_model.php
@@ -4,14 +4,14 @@
 			$this->load->database();
 		}
 
-		public function create_comment($cid){
-			$data = array (
-				'cid' => $cid,
-				'name' => $this->input->post('name'),
-				'email' => $this->input->post('email'),
-				'body' => $this->input->post('body'),
-				'rating' => $this->input->post('rating')
-			);
-			return $this->db->insert('Reviews', $data);
-		}
+//		public function create_comment($cid){
+//			$data = array (
+//				'cid' => $cid,
+//				'name' => $this->input->post('name'),
+//				'email' => $this->input->post('email'),
+//				'body' => $this->input->post('body'),
+//				'rating' => $this->input->post('rating')
+//			);
+//			return $this->db->insert('Reviews', $data);
+//		}
 	}
