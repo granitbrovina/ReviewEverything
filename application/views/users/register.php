8121a1c295d8dd4d608600193da1e82797103b3d
granit <granitbro@gmail.com>
2021-08-16 05:51:31 -0500

    signup form align center

--- application/views/users/register.php
+++ application/views/users/register.php
@@ -2,17 +2,21 @@
 <?php echo validation_errors(); ?>
 
 <?php echo form_open('users/register'); ?>
-	<div class="form-group">
-		<label>Name</label>
-		<input type="text" class="form-control" name="name" placeholder="Name">
-		<label>Email</label>
-		<input type="text" class="form-control" name="email" placeholder="Email">	
-		<label>Username</label>
-		<input type="text" class="form-control" name="username" placeholder="Username">	
-		<label>Password</label>
-		<input type="password" class="form-control" name="password" placeholder="Password">
-		<label>Confirm Password</label>
-		<input type="password" class="form-control" name="password2" placeholder="Confirm Password">
+<div class="row">
+	<div class="col-md-4 col-md-offset-4" style="margin: auto;">
+		<div class="form-group">
+			<label>Name</label>
+			<input type="text" class="form-control" name="name" placeholder="Name">
+			<label>Email</label>
+			<input type="text" class="form-control" name="email" placeholder="Email">	
+			<label>Username</label>
+			<input type="text" class="form-control" name="username" placeholder="Username">	
+			<label>Password</label>
+			<input type="password" class="form-control" name="password" placeholder="Password">
+			<label>Confirm Password</label>
+			<input type="password" class="form-control" name="password2" placeholder="Confirm Password">
+		</div>
 	</div>
+</div>
 	<button type="submit" class="btn btn-primary">Sign-up</button>
 <?php echo form_close(); ?>
