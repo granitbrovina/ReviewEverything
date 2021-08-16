f0a76bf1180aaba3528d5eae5a3c79adf37f0d45
granit <granitbro@gmail.com>
2021-08-16 06:52:42 -0500

    typo

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -16,7 +16,7 @@
 			$this->db->where('username', $username);
 			$this->db->where('password', $password);
 
-			$result = $this->db->get('users');
+			$result = $this->db->get('user');
 
 			if($result->num_rows() == 1){
 				return $result->row(0)->id;
