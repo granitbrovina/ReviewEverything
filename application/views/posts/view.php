fadd0ec20820c99ac31daa325697842398ac3f05
granit <granitbro@gmail.com>
2021-08-15 23:24:00 -0500

    patching and fixing review/comment capability

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -18,7 +18,7 @@
 		    <span class="star-cb-group">
 		      <input type="radio" id="rating-5" name="rating" value="5" />
 		      <label for="rating-5">5</label>
-		      <input type="radio" id="rating-4" name="rating" value="4" checked="checked" />
+		      <input type="radio" id="rating-4" name="rating" value="4" />
 		      <label for="rating-4">4</label>
 		      <input type="radio" id="rating-3" name="rating" value="3" />
 		      <label for="rating-3">3</label>
@@ -26,7 +26,7 @@
 		      <label for="rating-2">2</label>
 		      <input type="radio" id="rating-1" name="rating" value="1" />
 		      <label for="rating-1">1</label>
-		      <input type="radio" id="rating-0" name="rating" value="0" class="star-cb-clear" />
+		      <input type="radio" id="rating-0" name="rating" value="0" />
 		      <label for="rating-0">0</label>
 		    </span>
 		  </fieldset>
