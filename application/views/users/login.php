dbca9900fcbf2ae690595233a08a736beefca774
granit <granitbro@gmail.com>
2021-08-16 05:48:18 -0500

    login form align center

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -1,6 +1,6 @@
 <?php echo form_open('users/login'); ?>
 	<div class="row">
-		<div style="margin: auto;">
+		<div class="col-md-4 col-md-offset-4" style="margin: auto;">
 			<h1 class="text-center">Login</h1>
 			<div class="form-group">
 				<input type="text" name="username" class="form-control" placeholder="Enter Username" required autofocus>
