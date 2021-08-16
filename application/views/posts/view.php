8075ee92980de686cdd1bc28222d5d8ad818846e
granit <granitbro@gmail.com>
2021-08-16 02:35:27 -0500

    fixing stylesheet

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -12,12 +12,10 @@
 </form>
 <hr>
 <h3>Reviews</h3>
-<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+<div class="card" style="width: 100%">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
 <?php if($comments) : ?>
