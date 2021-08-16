82c1493bcd61c48c9fbbf0c2eb5438cbed5ebefb
granit <granitbro@gmail.com>
2021-08-15 22:26:51 -0500

    adding review/comment capability

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -10,3 +10,29 @@
 <?php echo form_open('/posts/delete/'.$post['cid']); ?>
 	<input type="submit" value="delete" class="btn btn-danger">
 </form>
+<hr>
+<h3>Add Review</h3>
+<?php echo form_open('comments/create'.$post['id']); ?>
+	<div class="form-group">
+		<label>Name</label>
+		<input type="text" name="name" class="form-control">
+	</div>
+	<div class="form-group">
+		<label>Email</label>
+		<input type="text" name="email" class="form-control">
+	</div>
+	<div class="form-group">
+		<label>Review</label>
+		<textarea type="text" class="form-control"></textarea>
+	</div>
+	<input type="hidden" name="slug" value="<?php echo $post['slug']; ?>">
+	<fieldset class="rating">
+    <legend>Please rate:</legend>
+    	<input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="Rocks!">5 stars</label>
+    	<input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="Pretty good">4 stars</label>
+    	<input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Meh">3 stars</label>
+    	<input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda bad">2 stars</label>
+    	<input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Sucks big time">1 star</label>
+	</fieldset>
+	<button class="btn btn-primary" type="submit"></button>
+</form>
