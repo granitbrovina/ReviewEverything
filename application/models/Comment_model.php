f5efcdb66b1c3e8f3e50196d909d72e51e663d43
granit <granitbro@gmail.com>
2021-08-16 01:42:46 -0500

    patching and fixing review/rating capability

--- application/models/Comment_model.php
+++ application/models/Comment_model.php
@@ -1,2 +1,17 @@
 <?php
-	
+	class Comment_model extends CI_Model{
+		public function __construct(){
+			$this->load->database();
+		}
+
+		public function create_comment($cid){
+			$data = array (
+				'cid' => $cid,
+				'name' => $this->input->post('name'),
+				'email' => $this->input->post('email'),
+				'body' => $this->input->post('body'),
+				'rating' => $this->input->post('rating')
+			);
+			return $this->db->insert('Reviews', $data);
+		}
+	}
